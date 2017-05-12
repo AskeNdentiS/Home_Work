3badebc9d8bfc39c7b7d0ce0309063aaef1302cf
Evgeniy <Askendentis@bigmir.net>
2017-05-12 20:01:07 +0300

    Проверка

--- src/com/source/it/lecture2/homework/TriangelSquare.java
+++ src/com/source/it/lecture2/homework/TriangelSquare.java
@@ -20,10 +20,6 @@ public class TriangelSquare {
 
         length = (float) lengthX1X2 + (float) lengthX1X3 + (float) lengthX2X3;
 
-        int z = 11111;
-        int s;
-
-
 
         System.out.println("Length of all sides is " + length);
         System.out.println("Square is " + square);
