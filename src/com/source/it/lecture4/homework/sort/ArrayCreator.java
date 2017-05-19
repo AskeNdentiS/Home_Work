b10124892451859687019d4cdd27658cc533dfe9
Evgeniy <Askendentis@bigmir.net>
2017-05-19 17:29:58 +0300

    Домашнее задание 4 sort fibonaccy

--- src/com/source/it/lecture4/homework/sort/ArrayCreator.java
+++ src/com/source/it/lecture4/homework/sort/ArrayCreator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ArrayCreator {
     public static int[] createHugeArray(int n) {
         Random random = new Random();
-        int[] result = new int[random.nextInt(10) + 10];
+        int[] result = new int[random.nextInt(1000000) + 1000000];
         for(int index = 0; index < result.length; index++) {
             result[index] = random.nextInt(n);
         }
--- src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
+++ src/com/source/it/lecture4/homework/sort/HugeArraySorter.java
@@ -8,15 +8,44 @@ public class HugeArraySorter {
 
         /*Your implementation here*/
 
-        System.out.println(Arrays.toString(hugeArray));
+       //System.out.println(Arrays.toString(hugeArray));
 
-        for (int i = 0; i<hugeArray.length; i++){
+       /** for (int i = 0; i<hugeArray.length; i++){
          for (int a = i+1; a<hugeArray.length;a++)
             if (hugeArray[a]<=hugeArray[i]){
                 hugeArray[a]=hugeArray[i] + hugeArray[a]-(hugeArray[i]=hugeArray[a]);
 
             }
+        }*/
+
+
+        /**int [] sortArray = new int[hugeArray.length+1];
+        for(int i = 0;i < hugeArray.length; i++) {
+            sortArray[i] = 0;
         }
+            for (int i = 0; i < hugeArray.length; i++) {
+                sortArray[hugeArray[i]]++;
+                }
+                    int s = 0;
+                for (int i = 0; i < hugeArray.length; i++) {
+                    for (int j = 0; j < sortArray[i]; j++) {
+                        hugeArray[s++] = i;
+                    }
+                }*/
+
+        int [] count = new int[hugeArray.length+1];
+        for (int elem : hugeArray){
+            count[elem]++;
+        }
+        int z = 0;
+        for (int i = 0; i < hugeArray.length; i++){
+            while ( count[i-0]>0){
+                hugeArray[z]=i;
+                z++;
+                count[i-0]--;
+            }
+        }
+
 
        for (int index = 0; index < hugeArray.length - 1; index++) {
             if (hugeArray[index] > hugeArray[index + 1]) {
@@ -26,6 +55,6 @@ public class HugeArraySorter {
         }
 
         System.out.println("Sort successful!!");
-        System.out.println(Arrays.toString(hugeArray));
+      // System.out.println(Arrays.toString(hugeArray));
     }
 }
